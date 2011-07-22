05c84d4e801162c44b1007aaea82b7877c90718f
david <david>
2011-07-22 14:44:50 +0000

    Minor cleanup.

--- createDatabaseTester.cpp
+++ createDatabaseTester.cpp
@@ -1,5 +1,5 @@
-//$Id: createDatabaseTester.cpp,v 1.2 2011/07/22 14:24:40 ferreira Exp $
-
+// $Id: createDatabaseTester.cpp,v 1.3 2011/07/22 14:44:50 david Exp $
+//
 // Copyright (C) 2008-2011 INESC ID Lisboa.
 //
 // This program is free software; you can redistribute it and/or modify
@@ -16,10 +16,12 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 //
-
-//$Log: createDatabaseTester.cpp,v $
-//Revision 1.2  2011/07/22 14:24:40  ferreira
-//Added Copyright comment
+// $Log: createDatabaseTester.cpp,v $
+// Revision 1.3  2011/07/22 14:44:50  david
+// Minor cleanup.
+//
+// Revision 1.2  2011/07/22 14:24:40  ferreira
+// Added Copyright comment
 //
 
 #include <vector>
